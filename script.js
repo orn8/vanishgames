f41b3ca99c5b0ac1e8c0e8be68dd36d09bc0afc7
oragne <me@oragne.dev>
2024-11-30 17:26:47 +1100

    app menu

--- script.js
+++ script.js
@@ -72,3 +72,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
   fetchAndSetupButtons();
 });
+
+// App Menu
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.getElementById("app-menu-button");
+  const menuGrid = document.getElementById("app-menu-grid");
+
+  menuButton.addEventListener("click", () => {
+    menuGrid.classList.toggle("show");
+  });
+
+  const appItems = document.querySelectorAll(".app-menu-item");
+
+  appItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      const link = item.dataset.link;
+      if (link) {
+        window.open(link, "_blank");
+      }
+    });
+  });
+});
